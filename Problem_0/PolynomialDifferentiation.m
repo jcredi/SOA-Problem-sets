08f78ef3a424ad99296d61f3a9d8b9c614c33c40
Jacopo Credi <jacopo.credi@gmail.com>
2015-09-07 14:00:39 +0200

    Should be done

--- Problem_0/PolynomialDifferentiation.m
+++ Problem_0/PolynomialDifferentiation.m
@@ -1,38 +1,30 @@
-function [derivativeCoefficients] = PolynomialDifferentiation(polynomialCoefficients, derivativeOrder)
+function [derivativeCoefficients] = PolynomialDifferentiation(polynomialCoefficients, k)
 %POLYNOMIALDIFFERENTIATION Compute derivative of polynomial function.
 %   Q = PolynomialDifferentiation(P,k) returns an array containing the
 %   polynomial coefficients (in ascending order) of the k-th order
 %   derivative of the polynomial function having coefficients specified in
 %   input array P.
 
+
 % Check for errors
-if ~exist('polynomialCoefficients','var') || ~exist('derivativeOrder','var')
-  fprintf('ERROR in function PolynomialDifferentiation.m\nNot enough input arguments. Execution will stop now. Returning NaN.\n');
+if nargin < 2 || ~isreal(polynomialCoefficients) || mod(k,1) || k < 0
+  fprintf('ERROR in function PolynomialDifferentiation.m : not enough or invalid input arguments.\n');
   derivativeCoefficients = nan;
-  return
-elseif ~isreal(polynomialCoefficients) || mod(derivativeOrder,1) || derivativeOrder < 0
-  fprintf('ERROR in function PolynomialDifferentiation.m\nInvalid input argument. Execution will stop now. Returning NaN.\n');
-  derivativeCoefficients = nan;
-  return
-elseif isempty(polynomialCoefficients)
-  fprintf('ERROR in function PolynomialDifferentiation.m\nThe given polynomial is empty. Execution will stop now. Returning empty polynomial.\n');
-  derivativeCoefficients = [];
   return
 end
 
 nCoefficients = length(polynomialCoefficients);
-derivativeCoefficients = nan(1,nCoefficients-1);
+derivativeCoefficients = zeros(1,max(1,nCoefficients-1));
 
-if derivativeOrder == 0 % trivial case
+if k == 0 % trivial case
   derivativeCoefficients = polynomialCoefficients;  
-  
-elseif derivativeOrder == 1 % base case
+elseif k == 1 % base case
   for i = 1:nCoefficients-1
       derivativeCoefficients(i) = i*polynomialCoefficients(i+1);
-  end    
-  
+  end     
 else % recursive case
-  derivativeCoefficients = PolynomialDifferentiation(PolynomialDifferentiation(polynomialCoefficients, derivativeOrder-1), 1);
+  kMinusOneThDerivative = PolynomialDifferentiation(polynomialCoefficients, k-1);
+  derivativeCoefficients = PolynomialDifferentiation(kMinusOneThDerivative, 1);
 end
 
 end
